898e9e24528cdcf7a2be9bb534309c11988f5379
Matthew Lock <himattlock@gmail.com>
2020-06-12 12:07:03 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+import Button from './components/Button/Button.js';
+
 
 function App() {
   return (
+    <div className="Container">
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Prompt</h1>
+        <p className="App-subtitle">daily stories for everyone, by anyone</p>
       </header>
-    </div>
+      <div className="App-library">
+        <div>
+          <h1>Buttons</h1>
+
+          <Button onClick={() => console.log('Button 1')}>
+            Button 1
+          </Button>
+
+          <Button type="gray" onClick={() => console.log('Button 2')}>
+            Button 2
+          </Button>
+
+          <Button type="primary" onClick={() => console.log('Button 3')}>
+            Button 3
+          </Button>
+        </div>
+        </div>
+</div>
+</div>
+      
+         
   );
 }
 
