7a47fec175c8a8f630c0b8a12272d49347cdf05c
Matthew Lock <himattlock@gmail.com>
2020-06-12 12:48:54 -0700

    heading fixes

--- src/App.js
+++ src/App.js
@@ -7,38 +7,21 @@ import Prompt from './components/Prompt/Prompt.js';
 
 function App() {
   return (
-    <div className="Container">
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Prompt</h1>
-        <p className="App-subtitle">daily stories for everyone, by anyone</p>
+        <h1>Prompt</h1>
+        <p>Daily stories for everyone, by anyone</p>
       </header>
 
       <div className="App-Prompt">
-        <div>
           <Prompt 
           date="june 12, 2020">
             <p>What is the meaning of life?</p>
             </Prompt>
+        </div> 
         </div>
-        {/* <div>
-          <h1>Buttons</h1>
 
-          <Button onClick={() => console.log('Button 1')}>
-            Button 1
-          </Button>
 
-          <Button type="gray" onClick={() => console.log('Button 2')}>
-            Button 2
-          </Button>
-
-          <Button type="primary" onClick={() => console.log('Button 3')}>
-            Button 3
-          </Button>
-        </div> */}
-        </div>
-</div>
-</div>
       
          
   );
