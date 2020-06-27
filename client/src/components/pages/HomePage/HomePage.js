9d06f1784a9562d2ced63bcbfbe9a5db5399ea80
vsri1137 <thenewvivek@gmail.com>
2020-06-26 21:21:18 -0700

    Front page

--- client/src/components/pages/HomePage/HomePage.js
+++ client/src/components/pages/HomePage/HomePage.js
@@ -1,88 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import './HomePage.css';
-
-
+import React, { useState, useEffect } from "react";
+import "./HomePage.css";
 
 function LandingPage(props) {
-const [userName, setUserName] = useState('');
-const [content, setContent] = useState('');
-const [prompts, setPrompts] = useState([]);
+  const [userName, setUserName] = useState("");
+  const [content, setContent] = useState("");
+  const [prompts, setPrompts] = useState([]);
 
-function onChangeContent(ev) {
-  setContent(ev.target.value);
-}
+  function onChangeContent(ev) {
+    setContent(ev.target.value);
+  }
 
-function onChangeUser(ev) {
-  setUserName(ev.target.value);
-}
+  function onChangeUser(ev) {
+    setUserName(ev.target.value);
+  }
 
-function todaysPrompt(){
-  return prompts.find(prompt => prompt.date === todaysDateString())
-}
+  function todaysPrompt() {
+    return prompts.find((prompt) => prompt.date === todaysDateString());
+  }
 
-function todaysDateString() {
-  const today = new Date()
-  const todayString = today.toLocaleString('default', {year: 'numeric', month: '2-digit',day: '2-digit'});
-  
-  return todayString.slice(0,10)
-}
+  function todaysDateString() {
+    const today = new Date();
+    const todayString = today.toLocaleString("default", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
 
-const date = new Date();  // 2009-11-10
-const cleanDate = date.toLocaleString('default', {year: 'numeric', month: '2-digit',day: '2-digit'});
-console.log(cleanDate);
+    return todayString.slice(0, 10);
+  }
 
+  const date = new Date(); // 2009-11-10
+  const cleanDate = date.toLocaleString("default", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+  console.log(cleanDate);
 
-function submit() {
-  const formData = {
-    // user: userName,
-    text: content,
-  };
+  function submit() {
+    const formData = {
+      // user: userName,
+      text: content,
+    };
 
-  // Can also be written:
-  // const formData = {title, text: content};
+    // Can also be written:
+    // const formData = {title, text: content};
 
-  fetch('/api/dailyquestions/add-response/?_id=' + todaysPrompt()._id, {
-      method: 'PUT',
-      headers: {'Content-Type': 'application/json'},
+    fetch("/api/dailyquestions/add-response/?_id=" + todaysPrompt()._id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Got this back', data);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Got this back", data);
 
-      // Redirect to blog
-      props.history.push('/blog/');
-    });
-    }
+        // Redirect to blog
+        props.history.push("/blog/");
+      });
+  }
 
   function fetchPrompts() {
-    console.log('Fetching date from API');
-    fetch('/api/mongodb/dailyquestions/')
-      .then(response => response.json())
-      .then(data => {
-        console.log('Got a question back', data);
+    console.log("Fetching date from API");
+    fetch("/api/mongodb/dailyquestions/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Got a question back", data);
         window.myData = data;
         setPrompts(data);
       });
-    }
-      
+  }
+
   useEffect(fetchPrompts, []);
 
+  const prompt = todaysPrompt();
 
+  return (
+    <div className="WriteArticle">
+      <h1>Inspiration found</h1>
 
-const prompt = todaysPrompt()
+      <p>A good story is like fire, it takes a spark to get started. </p>
 
-return (
-  <div className="WriteArticle">
-    <h1>Love to write?</h1>
-    <p>Do you love to write?  Do you want to become a better writer?  Do you often not know what to write about?  Well, you've come to the right place!</p>
-
-    <p>Here, at Daily Milk we provide you with a new writing prompt every day that will get your creative juices flowing.  Simply write your own story, based of the daily prompt and satiate your writing craving.  Come back every day for a new prompt and to read all the responses from yesterday's prompt.</p>
-  </div>
-  
-
-
-);
+      <p>At Daily Milk, you'll find a new writing prompt every day to kickstart
+        your writing process. Once you've shared your story, you can browse the
+        stories of other writers in the Daily Milk community.
+      </p>
+      <a href="/dailyprompt">Get started</a>
+    </div>
+  );
 }
 
 export default LandingPage;
